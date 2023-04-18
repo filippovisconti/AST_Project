9f9aa717243c3eba02ce5b1f6620d3206a6d73f3
Filippo Visconti <filippo.visconti23@gmail.com>
2023-04-19 00:01:13 +0200

    Spawns containers and runs a test-playbook successfully

--- Python/docker_utilities.py
+++ Python/docker_utilities.py
@@ -1,3 +1,5 @@
+import os
+import pprint
 from click import pause
 import docker, logging
 
@@ -17,6 +19,7 @@ image_name = 'debian'
 container_count = 3
 platform = 'linux/arm64'
 start_command = 'tail -f /dev/null'
+cwd = os.getcwd()
 
 
 def create_network():
@@ -51,6 +54,16 @@ def create_cnc_macine():
                                          network=network_name,
                                          platform=platform,
                                          command=start_command,
+                                         volumes={
+                                             f'{cwd}/ansible': {
+                                                 'bind': '/root/ansible',
+                                                 'mode': 'rw'
+                                             },
+                                             f'{cwd}/ansible/ansible.cfg': {
+                                                 'bind': '/root/.ansible.cfg',
+                                                 'mode': 'rw'
+                                             }
+                                         },
                                          detach=True,
                                          dns=['8.8.8.8', '8.8.4.4'])
 
@@ -58,24 +71,46 @@ def create_cnc_macine():
 
     container.start()
     container.reload()
+
+    command = 'chmod 600 /root/ansible/ansible_ed25519'
+    container.exec_run(command)
+
     logging.info(
         f'Started container {container_name} with IP address {container.attrs["NetworkSettings"]["Networks"][network_name]["IPAddress"]}'
     )
 
+    # res = container.exec_run(
+    #     'ansible-playbook -i /root/ansible/inventory.ini /root/ansible/test_playbook.yaml'  # > /root/ansible/output.txt'
+    # )
+    # print(res)
+
 
 # apt update && apt install net-tools && ifconfig
+# ansible-playbook -i inventory.ini test_playbook.yaml
+# chmod 600 ansible_ed25519
 # Create function to create containers
 def create_containers():
     # Create containers
     for i in range(container_count):
         container_name = f'container_{i+2}'
-        container = client.containers.create(image_name,
-                                             name=container_name,
-                                             network=network_name,
-                                             platform=platform,
-                                             command=start_command,
-                                             detach=True,
-                                             dns=['8.8.8.8', '8.8.4.4'])
+        container = client.containers.create(
+            image_name,
+            name=container_name,
+            network=network_name,
+            platform=platform,
+            command=start_command,
+            volumes={
+                f'{cwd}/ansible/keys': {
+                    'bind': '/root/.ssh/',
+                    'mode': 'rw'
+                },
+                f'{cwd}/ansible/sshd_config.d': {
+                    'bind': '/etc/ssh/sshd_config.d/',
+                    'mode': 'rw'
+                }
+            },
+            detach=True,
+            dns=['8.8.8.8', '8.8.4.4'])
 
         logging.info(f'Created container {container_name}')
 
@@ -84,6 +119,16 @@ def create_containers():
         logging.info(
             f'Started container {container_name} with IP address {container.attrs["NetworkSettings"]["Networks"][network_name]["IPAddress"]}'
         )
+        command = 'apt update'
+        res = container.exec_run(command)
+        command = 'apt install net-tools openssh-server python3 sudo -y'
+        res = container.exec_run(command)
+        command = 'service ssh start'
+        res = container.exec_run(command)
+        command = 'passwd -d root'
+        res = container.exec_run(command)
+
+        logging.info(f'Executed command on container {container_name}')
 
 
 def delete_containers():
@@ -103,10 +148,19 @@ def main():
     try:
         create_network()
         build_ansible_image()
-        create_cnc_macine()
         create_containers()
+        create_cnc_macine()
+
         # Wait for user input
-        pause()
+        # pause()
+        res = client.containers.get('cnc_machine').exec_run(
+            'ansible-playbook -i /root/ansible/inventory.ini /root/ansible/test_playbook.yaml'  # > /root/ansible/output.txt'
+        )
+        if res.exit_code == 0:
+            logging.info("Ansible playbook executed successfully")
+        else:
+            logging.error(f"Ansible playbook failed - code {res.exit_code}")
+            logging.error(res.output.decode('utf-8'))
 
     except:
         logging.exception("Exception occurred. Cleaning up...")
